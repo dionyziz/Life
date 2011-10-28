4e175bdf4235530591b0c30886e09a2b3f8d2fee
dionyziz <dionyziz@kamibu.com>
2011-10-28 20:41:20 +0000

    sample settings file

---
+++ settings-sample.php
@@ -0,0 +1,10 @@
+<?php
+    return array(
+        'db' => array(
+            'user' => 'life',
+            'password' => 'your-password',
+            'name' => 'life'
+        ),
+        'url' => 'http://example.com/'
+    );
+?>
