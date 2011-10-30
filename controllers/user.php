5d53384c837bfd2b7e96eb72041e07ca99a1509e
Themis Papameletiou <themicp@gmail.com>
2011-10-30 01:09:47 +0100

    file authentication

--- controllers/user.php
+++ controllers/user.php
@@ -17,7 +17,7 @@
             catch ( UserException $e ) {
                 Redirect( 'user/create?error=noregister' );
             }
-            echo $credentials[ 'password' ];
+            var_dump( $credentials );
             $_SESSION[ 'user' ] = User::item( $credentials[ 'id' ] );
         }
         public function delete( $id ) {
